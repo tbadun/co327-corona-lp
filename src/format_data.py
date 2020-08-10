c1b2c9c3bcade628979340efd6ef4d10b21ceb29
tbadun <58403128+tbadun@users.noreply.github.com>
2020-08-10 01:11:56 -0400

    test w/o shipped to as of yesterday constraint on availability

--- src/format_data.py
+++ src/format_data.py
@@ -168,7 +168,7 @@ def availability_recipes(shipping):
 
     # all in/outflow
     shipped_from = {("z_{}_{}->{}_{}".format(material,start,end,day),"availability_{}_{}_{}".format(material,place,day)): 1 for day in range(1,days) for start,end,cap,cost in shipping for material in materials for place in places if place == start}
-    shipped_to_yest = {("z_{}_{}->{}_{}".format(material,start,end,day-1),"availability_{}_{}_{}".format(material,place,day)): -1 for day in range(2,days) for start,end,cap,cost in shipping for material in materials for place in places if place == end}
+    # shipped_to_yest = {("z_{}_{}->{}_{}".format(material,start,end,day-1),"availability_{}_{}_{}".format(material,place,day)): -1 for day in range(2,days) for start,end,cap,cost in shipping for material in materials for place in places if place == end}
     
     # available to ship
     onhand = {("M_{}_{}_{}".format(material,place,day),"availability_{}_{}_{}".format(material,place,day)): -1 for material in materials for place in places for day in range(1,days)}
